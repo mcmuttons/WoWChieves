3d8298c16d9a221bab7e9ba76a80fd1d49316d72
Christian Abildsoe <mcmuttons@gmail.com>
2011-10-08 19:17:30 +0200

    Initial test code

---
+++ Database/DatabaseReader.fs
@@ -0,0 +1,17 @@
+﻿module DatabaseReader
+
+open System.Data.SqlServerCe
+
+let path = "E:\\Development\\WoW Achievements\\WoWChieves\\"
+let file = "wowchieves.sdf"
+let source = "Datasource=" + path + file + ";"
+let connection = new SqlCeConnection(source)
+
+
+let command = new SqlCeCommand("SELECT * FROM TestTable", connection)
+let result = 
+    connection.Open()
+    command.ExecuteResultSet(ResultSetOptions.Scrollable)
+
+
+
---
+++ DatabaseTest/DatabaseReaderTest.fs
@@ -0,0 +1,19 @@
+﻿module DatabaseReaderTest
+
+open NUnit.Framework
+open FsUnit
+
+
+[<TestFixture>]
+type DatabaseReaderTest() =
+    let result = DatabaseReader.result
+
+    [<Test>] 
+    member x.ResultsAreReturnedFromDatabase() =
+        result.HasRows |> should equal true
+
+    [<Test>]
+    member x.FirstRowContainsCorrectValues() =
+        if result.ReadFirst() then
+            result.GetInt32(0)  |> should equal 1
+            result.GetString(1) |> should equal "First"
